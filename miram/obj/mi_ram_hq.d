549742266345722b56f1535e617b2d0fe0d52b77
Alesanj <alejandro.sanjinez1@gmail.com>
2021-07-03 14:22:36 -0300

    asd

---
+++ miram/obj/mi_ram_hq.d
@@ -0,0 +1,11 @@
+obj/mi_ram_hq.d: src/mi_ram_hq.c src/mi_ram_hq.h \
+ ../shared/shared/conexion.h ../shared/shared/TAD_PATOTA.h \
+ ../shared/shared/TAD_TRIPULANTE.h
+
+src/mi_ram_hq.h:
+
+../shared/shared/conexion.h:
+
+../shared/shared/TAD_PATOTA.h:
+
+../shared/shared/TAD_TRIPULANTE.h:
